70717ec53f7430c9f1c4798c77a00678644614a7
Mohammad Abrar Wadud <abrar.discloses@gmail.com>
2026-01-27 23:19:47 -0600

    Add ZZ_CR tree snapshot branches

---
+++ PlotsConfigurationsRun3/ZH_4lMET/ZZ_CR/cuts.py
@@ -0,0 +1,22 @@
+cuts = {}
+
+preselections = ""
+preselections += "nLepton >= 4"
+preselections += " && Alt(Lepton_pt, 0, 0) > 25"
+preselections += " && Alt(Lepton_pt, 1, 0) > 15"
+preselections += " && Alt(Lepton_pt, 2, 0) > 10"
+preselections += " && Alt(Lepton_pt, 3, 0) > 10"
+preselections += " && Alt(Lepton_pt, 4, 0) < 10"
+preselections += " && Z0_mass > 12"
+preselections += " && bVeto"
+preselections += " && sumLeptonCharge == 0"
+
+cuts["zz_cr"] = {
+    "expr": "abs(Z0_mass - 91.1876) < 15 && X_mass > 75 && X_mass < 105 && PuppiMET_pt < 35",
+    "categories": {
+        "XSF_ZEE": "X_isSF && Z0_isEE",
+        "XSF_ZMM": "X_isSF && Z0_isMM",
+        "XDF_ZEE": "X_isDF && Z0_isEE",
+        "XDF_ZMM": "X_isDF && Z0_isMM",
+    },
+}
