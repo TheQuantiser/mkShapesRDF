5eb4a94b24336f5580e9c2f45fc364441cfd7ed6
Mohammad <mohammad.abrar.wadud@cern.ch>
2026-02-12 18:23:28 +0100

    update

--- PlotsConfigurationsRun3/ZH_4lMET/ZZ_CR/variables.py
+++ PlotsConfigurationsRun3/ZH_4lMET/ZZ_CR/variables.py
@@ -1,12 +1,5 @@
 variables = {}
 
-pair_leptons = [
-    ("lZ1", "Alt(Z0_idx, 0, -1)"),
-    ("lZ2", "Alt(Z0_idx, 1, -1)"),
-    ("lX1", "Alt(X_idx, 0, -1)"),
-    ("lX2", "Alt(X_idx, 1, -1)"),
-]
-
 BASE_EVENT_BRANCHES = [
     "nCleanJet",
     "Z0_mass",
@@ -28,6 +21,15 @@ BASE_EVENT_BRANCHES = [
     "bVeto",
 ]
 
+tree_branches = {branch: branch for branch in BASE_EVENT_BRANCHES}
+
+pair_leptons = [
+    ("lZ1", "Alt(Z0_idx, 0, -1)"),
+    ("lZ2", "Alt(Z0_idx, 1, -1)"),
+    ("lX1", "Alt(X_idx, 0, -1)"),
+    ("lX2", "Alt(X_idx, 1, -1)"),
+]
+
 LEPTON_BRANCH_RECIPES = {
     "pt": "Lepton_pt",
     "eta": "Lepton_eta",
@@ -38,15 +40,6 @@ LEPTON_BRANCH_RECIPES = {
     "genPt": "Lepton_genPt",
     "genEta": "Lepton_genEta",
     "genPhi": "Lepton_genPhi",
-}
-
-JET_BRANCH_RECIPES = {
-    "pt": ("CleanJet_pt", "{jet_idx}", "0"),
-    "eta": ("CleanJet_eta", "{jet_idx}", "0"),
-    "phi": ("CleanJet_phi", "{jet_idx}", "0"),
-    "genPt": ("GenJet_pt", "{clean_jet_gen_idx}", "-999"),
-    "genEta": ("GenJet_eta", "{clean_jet_gen_idx}", "0"),
-    "genPhi": ("GenJet_phi", "{clean_jet_gen_idx}", "0"),
 }
 
 electron_tight_wps_2022 = [
@@ -78,15 +71,6 @@ TIGHT_OBJECT_CONFIG = {
     },
 }
 
-tree_branches = {branch: branch for branch in BASE_EVENT_BRANCHES}
-
-for jet_idx in range(2):
-    clean_jet_gen_idx = f"Alt(Jet_genJetIdx, Alt(CleanJet_jetIdx, {jet_idx}, -1), -1)"
-    for suffix, (source, index_expr, default) in JET_BRANCH_RECIPES.items():
-        tree_branches[f"CleanJet_{suffix}_{jet_idx}"] = (
-            f"Alt({source}, {index_expr.format(jet_idx=jet_idx, clean_jet_gen_idx=clean_jet_gen_idx)}, {default})"
-        )
-
 for lep_label, lep_idx in pair_leptons:
     for suffix, source in LEPTON_BRANCH_RECIPES.items():
         tree_branches[f"{lep_label}_{suffix}"] = f"Alt({source}, {lep_idx}, 0)"
@@ -101,6 +85,22 @@ for obj_name, obj_cfg in TIGHT_OBJECT_CONFIG.items():
     for wp in obj_cfg["wps"]:
         tree_branches[f"nTight{obj_name}_{wp}"] = (
             f"Sum((abs(Lepton_pdgId) == {obj_cfg['pdg_id']}) && ({obj_cfg['flag_branch']}_{wp} > 0.5))"
+        )
+
+JET_BRANCH_RECIPES = {
+    "pt": ("CleanJet_pt", "{jet_idx}", "0"),
+    "eta": ("CleanJet_eta", "{jet_idx}", "0"),
+    "phi": ("CleanJet_phi", "{jet_idx}", "0"),
+    "genPt": ("GenJet_pt", "{clean_jet_gen_idx}", "-999"),
+    "genEta": ("GenJet_eta", "{clean_jet_gen_idx}", "0"),
+    "genPhi": ("GenJet_phi", "{clean_jet_gen_idx}", "0"),
+}
+
+for jet_idx in range(2):
+    clean_jet_gen_idx = f"Alt(Jet_genJetIdx, Alt(CleanJet_jetIdx, {jet_idx}, -1), -1)"
+    for suffix, (source, index_expr, default) in JET_BRANCH_RECIPES.items():
+        tree_branches[f"CleanJet_{suffix}_{jet_idx}"] = (
+            f"Alt({source}, {index_expr.format(jet_idx=jet_idx, clean_jet_gen_idx=clean_jet_gen_idx)}, {default})"
         )
 
 # Variable-to-branch conversion
